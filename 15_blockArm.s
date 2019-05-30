ea1ab3de8117346bf7d52b43069350c397bc7d8d
梁泽 <350442340@qq.com>
2019-05-30 10:17:08 +0800

    no message

---
+++ 15_blockArm.s
@@ -0,0 +1,299 @@
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     -[ViewController click1:]:
+0000000100006598         stp        x20, x19, [sp, #-0x20]!                     ; Objective C Implementation defined at 0x1000083c0 (instance method), DATA XREF=0x1000083c0
+000000010000659c         stp        x29, x30, [sp, #0x10]
+00000001000065a0         add        x29, sp, #0x10
+00000001000065a4         adr        x0, #0x1000082c0                            ; @"click1"
+00000001000065a8         nop
+00000001000065ac         bl         imp___stubs__NSLog
+00000001000065b0         nop
+00000001000065b4         ldr        x19, #0x100008000
+00000001000065b8         adr        x1, #0x1000080c0
+00000001000065bc         nop
+00000001000065c0         mov        x0, x19
+00000001000065c4         bl         imp___stubs__dispatch_async
+00000001000065c8         adr        x0, #0x1000082e0                            ; @"click2"
+00000001000065cc         nop
+00000001000065d0         bl         imp___stubs__NSLog
+00000001000065d4         adr        x1, #0x1000081c0
+00000001000065d8         nop
+00000001000065dc         mov        x0, x19
+00000001000065e0         ldp        x29, x30, [sp, #0x10]
+00000001000065e4         ldp        x20, x19, [sp]!, #0x20
+00000001000065e8         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_1000065ec:
+00000001000065ec         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x1000080c0, DATA XREF=0x1000080d0
+00000001000065f0         mov        x29, sp
+00000001000065f4         adr        x0, #0x1000082c0                            ; @"click1"
+00000001000065f8         nop
+00000001000065fc         bl         imp___stubs__NSLog
+0000000100006600         nop
+0000000100006604         ldr        x0, #0x100008000
+0000000100006608         adr        x1, #0x1000080e0
+000000010000660c         nop
+0000000100006610         ldp        x29, x30, [sp]!, #0x10
+0000000100006614         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_100006618:
+0000000100006618         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x1000080e0, DATA XREF=0x1000080f0
+000000010000661c         mov        x29, sp
+0000000100006620         adr        x0, #0x1000082c0                            ; @"click1"
+0000000100006624         nop
+0000000100006628         bl         imp___stubs__NSLog
+000000010000662c         nop
+0000000100006630         ldr        x0, #0x100008000
+0000000100006634         adr        x1, #0x100008100
+0000000100006638         nop
+000000010000663c         ldp        x29, x30, [sp]!, #0x10
+0000000100006640         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_100006644:
+0000000100006644         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008100, DATA XREF=0x100008110
+0000000100006648         mov        x29, sp
+000000010000664c         adr        x0, #0x1000082c0                            ; @"click1"
+0000000100006650         nop
+0000000100006654         bl         imp___stubs__NSLog
+0000000100006658         nop
+000000010000665c         ldr        x0, #0x100008000
+0000000100006660         adr        x1, #0x100008120
+0000000100006664         nop
+0000000100006668         ldp        x29, x30, [sp]!, #0x10
+000000010000666c         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_100006670:
+0000000100006670         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008120, DATA XREF=0x100008130
+0000000100006674         mov        x29, sp
+0000000100006678         adr        x0, #0x1000082c0                            ; @"click1"
+000000010000667c         nop
+0000000100006680         bl         imp___stubs__NSLog
+0000000100006684         nop
+0000000100006688         ldr        x0, #0x100008000
+000000010000668c         adr        x1, #0x100008140
+0000000100006690         nop
+0000000100006694         ldp        x29, x30, [sp]!, #0x10
+0000000100006698         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_10000669c:
+000000010000669c         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008140, DATA XREF=0x100008150
+00000001000066a0         mov        x29, sp
+00000001000066a4         adr        x0, #0x1000082c0                            ; @"click1"
+00000001000066a8         nop
+00000001000066ac         bl         imp___stubs__NSLog
+00000001000066b0         nop
+00000001000066b4         ldr        x0, #0x100008000
+00000001000066b8         adr        x1, #0x100008160
+00000001000066bc         nop
+00000001000066c0         ldp        x29, x30, [sp]!, #0x10
+00000001000066c4         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_1000066c8:
+00000001000066c8         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008160, DATA XREF=0x100008170
+00000001000066cc         mov        x29, sp
+00000001000066d0         adr        x0, #0x1000082c0                            ; @"click1"
+00000001000066d4         nop
+00000001000066d8         bl         imp___stubs__NSLog
+00000001000066dc         nop
+00000001000066e0         ldr        x0, #0x100008000
+00000001000066e4         adr        x1, #0x100008180
+00000001000066e8         nop
+00000001000066ec         ldp        x29, x30, [sp]!, #0x10
+00000001000066f0         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_1000066f4:
+00000001000066f4         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008180, DATA XREF=0x100008190
+00000001000066f8         mov        x29, sp
+00000001000066fc         adr        x0, #0x1000082c0                            ; @"click1"
+0000000100006700         nop
+0000000100006704         bl         imp___stubs__NSLog
+0000000100006708         nop
+000000010000670c         ldr        x0, #0x100008000
+0000000100006710         adr        x1, #0x1000081a0
+0000000100006714         nop
+0000000100006718         ldp        x29, x30, [sp]!, #0x10
+000000010000671c         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_100006720:
+0000000100006720         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x1000081a0, DATA XREF=0x1000081b0
+0000000100006724         mov        x29, sp
+0000000100006728         orr        w0, wzr, #0x1
+000000010000672c         bl         imp___stubs__exit
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_100006730:
+0000000100006730         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x1000081c0, DATA XREF=0x1000081d0
+0000000100006734         mov        x29, sp
+0000000100006738         adr        x0, #0x1000082e0                            ; @"click2"
+000000010000673c         nop
+0000000100006740         bl         imp___stubs__NSLog
+0000000100006744         nop
+0000000100006748         ldr        x0, #0x100008000
+000000010000674c         adr        x1, #0x1000081e0
+0000000100006750         nop
+0000000100006754         ldp        x29, x30, [sp]!, #0x10
+0000000100006758         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_10000675c:
+000000010000675c         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x1000081e0, DATA XREF=0x1000081f0
+0000000100006760         mov        x29, sp
+0000000100006764         adr        x0, #0x1000082e0                            ; @"click2"
+0000000100006768         nop
+000000010000676c         bl         imp___stubs__NSLog
+0000000100006770         nop
+0000000100006774         ldr        x0, #0x100008000
+0000000100006778         adr        x1, #0x100008200
+000000010000677c         nop
+0000000100006780         ldp        x29, x30, [sp]!, #0x10
+0000000100006784         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_100006788:
+0000000100006788         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008200, DATA XREF=0x100008210
+000000010000678c         mov        x29, sp
+0000000100006790         adr        x0, #0x1000082e0                            ; @"click2"
+0000000100006794         nop
+0000000100006798         bl         imp___stubs__NSLog
+000000010000679c         nop
+00000001000067a0         ldr        x0, #0x100008000
+00000001000067a4         adr        x1, #0x100008220
+00000001000067a8         nop
+00000001000067ac         ldp        x29, x30, [sp]!, #0x10
+00000001000067b0         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_1000067b4:
+00000001000067b4         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008220, DATA XREF=0x100008230
+00000001000067b8         mov        x29, sp
+00000001000067bc         adr        x0, #0x1000082e0                            ; @"click2"
+00000001000067c0         nop
+00000001000067c4         bl         imp___stubs__NSLog
+00000001000067c8         nop
+00000001000067cc         ldr        x0, #0x100008000
+00000001000067d0         adr        x1, #0x100008240
+00000001000067d4         nop
+00000001000067d8         ldp        x29, x30, [sp]!, #0x10
+00000001000067dc         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_1000067e0:
+00000001000067e0         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008240, DATA XREF=0x100008250
+00000001000067e4         mov        x29, sp
+00000001000067e8         adr        x0, #0x1000082e0                            ; @"click2"
+00000001000067ec         nop
+00000001000067f0         bl         imp___stubs__NSLog
+00000001000067f4         nop
+00000001000067f8         ldr        x0, #0x100008000
+00000001000067fc         adr        x1, #0x100008260
+0000000100006800         nop
+0000000100006804         ldp        x29, x30, [sp]!, #0x10
+0000000100006808         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_10000680c:
+000000010000680c         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008260, DATA XREF=0x100008270
+0000000100006810         mov        x29, sp
+0000000100006814         adr        x0, #0x1000082e0                            ; @"click2"
+0000000100006818         nop
+000000010000681c         bl         imp___stubs__NSLog
+0000000100006820         nop
+0000000100006824         ldr        x0, #0x100008000
+0000000100006828         adr        x1, #0x100008280
+000000010000682c         nop
+0000000100006830         ldp        x29, x30, [sp]!, #0x10
+0000000100006834         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_100006838:
+0000000100006838         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x100008280, DATA XREF=0x100008290
+000000010000683c         mov        x29, sp
+0000000100006840         adr        x0, #0x1000082e0                            ; @"click2"
+0000000100006844         nop
+0000000100006848         bl         imp___stubs__NSLog
+000000010000684c         nop
+0000000100006850         ldr        x0, #0x100008000
+0000000100006854         adr        x1, #0x1000082a0
+0000000100006858         nop
+000000010000685c         ldp        x29, x30, [sp]!, #0x10
+0000000100006860         b          imp___stubs__dispatch_async
+                        ; endp
+
+
+        ; ================ B E G I N N I N G   O F   P R O C E D U R E ================
+
+
+                     sub_100006864:
+0000000100006864         stp        x29, x30, [sp, #-0x10]!                     ; Objective C Block defined at 0x1000082a0, DATA XREF=0x1000082b0
+0000000100006868         mov        x29, sp
+000000010000686c         bl         imp___stubs__abort
+                        ; endp
